44295430ccb684557b5c50cbcfa78a99fa752ae8
Ken Ken <kiannlayam@gmail.com>
2024-05-01 21:16:55 +0800

    modified products

--- JeekobsSportCenter_ADMIN/JAVASCRIPT/Logout_Soccerball.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#show-logout").addEventListener("click",function(){
-    document.querySelector(".Form").classList.add("active");
-});
-document.querySelector(".Form .close-btn").addEventListener("click",function(){
-    document.querySelector(".Form").classList.remove("active");
-});
-
